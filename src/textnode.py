12818ac0aba40f06242de93baa2bb4b9dfef5e30
D.A.F. <dafonda.code@gmail.com>
2025-02-07 14:56:15 +0000

    Initial commit of base setup

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    UNDERLINE = "underline"
+    STRIKETHROUGH = "strikethrough"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text: str, type: TextType):
+        self.text = text
+        self.text_type = type
+        self.url = None
+
+    def __eq__(self, other: "TextNode") -> bool:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
